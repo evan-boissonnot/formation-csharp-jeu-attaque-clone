07da83c85abd265afc6ac0a8abd97b93a3c17db2
evan-boissonnot <commercial@boissonnot.Fr>
2019-04-30 16:34:30 +0200

    premier projet mvc.net

---
+++ JeuDroides.Web.UI/Controllers/WookieController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JeuDroides.Web.UI.Controllers
+{
+    public class WookieController : Controller
+    {
+        #region Public methods
+        public IActionResult Index()
+        {
+            List<string> wookies = new List<string>() {
+                "Chewie",
+                "Attichitcuk",
+                "Bacca",
+                "Chak"
+            };
+
+            this.ViewBag.ListeWookies = wookies;
+
+            return this.View();
+        }
+        #endregion
+    }
+}
---
+++ MonPremierProjetAspnetCore/Controllers/YodaController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MonPremierProjetAspnetCore.Models;
+
+namespace MonPremierProjetAspnetCore.Controllers
+{
+    public class YodaController : Controller
+    {
+        public IActionResult Index()
+        {
+            this.ViewBag.NombreDeGlaces = 4;
+            this.ViewData["NombreDeGlaces"] = 4;
+
+
+
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            List<string> images = new List<string>() 
+            {
+                "http://img.over-blog.com/375x500/3/98/73/39/Fonds-d-ecran-Ipad/Deux-parfums-jpg-fond-d-ecran-gratuit-pour-Ipad-2-et-Ipad.jpeg",
+                "http://img.over-blog.com/375x500/3/98/73/39/Fonds-d-ecran-Ipad/Deux-parfums-jpg-fond-d-ecran-gratuit-pour-Ipad-2-et-Ipad.jpeg",
+                "http://img.over-blog.com/375x500/3/98/73/39/Fonds-d-ecran-Ipad/Deux-parfums-jpg-fond-d-ecran-gratuit-pour-Ipad-2-et-Ipad.jpeg",
+                "http://img.over-blog.com/375x500/3/98/73/39/Fonds-d-ecran-Ipad/Deux-parfums-jpg-fond-d-ecran-gratuit-pour-Ipad-2-et-Ipad.jpeg"
+            };
+
+            this.ViewBag.ListePhotos = images;
+
+
+            return View();
+        }
+
+        public IActionResult IndexAvecModel()
+        {
+            List<Glace> listGlaces = new List<Glace>() {
+                new Glace() { Url =  "http://img.over-blog.com/375x500/3/98/73/39/Fonds-d-ecran-Ipad/Deux-parfums-jpg-fond-d-ecran-gratuit-pour-Ipad-2-et-Ipad.jpeg" },
+                new Glace() { Url =  "http://img.over-blog.com/375x500/3/98/73/39/Fonds-d-ecran-Ipad/Deux-parfums-jpg-fond-d-ecran-gratuit-pour-Ipad-2-et-Ipad.jpeg" },
+                new Glace() { Url =  "http://img.over-blog.com/375x500/3/98/73/39/Fonds-d-ecran-Ipad/Deux-parfums-jpg-fond-d-ecran-gratuit-pour-Ipad-2-et-Ipad.jpeg" },
+                new Glace() { Url =  "http://img.over-blog.com/375x500/3/98/73/39/Fonds-d-ecran-Ipad/Deux-parfums-jpg-fond-d-ecran-gratuit-pour-Ipad-2-et-Ipad.jpeg" }
+            };
+
+            return View(listGlaces);
+        }
+    }
+}
